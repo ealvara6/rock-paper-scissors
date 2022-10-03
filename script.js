7b2edaacd8b2b225ecef9d11da62445cab435511
Eduardo Alvarado <ealvara73@gmail.com>
2022-10-03 18:37:05 -0500

    Create game function

--- script.js
+++ script.js
@@ -3,47 +3,64 @@
 let getComputerChoice = () => {
     let choices = ['rock', 'paper', 'scissors'];
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(computerChoice);
+    return computerChoice;
 }
 
 let playRound = (computerSelection, playerSelection) => {
-    let result = "";
+    let result;
     if(computerSelection === 'rock') {
         switch(playerSelection) {
             case "paper":
-                result = "Paper beats rock. You win the round!";
+                result = ["Paper beats rock. You win the round!", "player"];
                 break;
             case "scissors":
-                result = "Rock beats scissors. You lose the round.";
+                result = ["Rock beats scissors. You lose the round.", "computer"];
                 break;
             default:
-                result = "It's a tie!";
+                result = ["It's a tie!", "tie"];
         }
     }
     if(computerSelection === "scissors") {
         switch(playerSelection) {
             case "rock":
-                result = "Rock beats scissors. You win the round!";
+                result = ["Rock beats scissors. You win the round!", "player"];
                 break;
             case "paper":
-                result = "Scissors beats paper. You lose the round.";
+                result = ["Scissors beats paper. You lose the round.", "computer"];
                 break;
             default:
-                result = "It's a tie!";
+                result = ["It's a tie!", "tie"];
         }
     }
     if(computerSelection === "paper") {
         switch(playerSelection) {
             case "scissors":
-                result = "Scissors beats paper. You win the round!"
+                result = ["Scissors beats paper. You win the round!", "player"];
                 break;
             case "rock":
-                result = "Paper beats rock. You lose the round.";
+                result = ["Paper beats rock. You lose the round.", "computer"];
                 break;
             default:
-                result = "It's a tie!";
+                result = ["It's a tie!", "tie"];
         }
+    }
+
+    return result;
+}
+
+let game = () => {
+    alert("Play Rock Paper Scissors against a computer in a best of 5.");
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++){
+        let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+        let computerSelection = getComputerChoice();
+        
+        console.log(`Computer's Choice: ${computerSelection}\nPlayer's Choice: ${playerSelection}`.toUpperCase());
+        let result = playRound(computerSelection, playerSelection);
+        console.log(result[0]);
     }
 }
 
-let computerSelection = getComputerChoice();
+game();
